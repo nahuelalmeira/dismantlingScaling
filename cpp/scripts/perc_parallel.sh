7136d4e949c76dfc28afb0010ddd451d5e69b848
nahuelalmeira <nahu1990@gmail.com>
2019-08-14 12:47:55 -0300

    Added parallel support for perc

---
+++ cpp/scripts/perc_parallel.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Example: echo $(seq 1 8) | xargs -n 1 -P 4 ./perc_parallel.sh ER_k5.00 16000 Ran True
+
+BASE_DIR="../../networks/ER"
+
+BASE_NET=$1
+N=$2
+ATTACK=$3
+OVERWRITE=$4
+SEED=$( printf %05d $5 )
+
+BASE_NET_DIR="${BASE_DIR}/${BASE_NET}/${BASE_NET}_N${N}"
+
+NET_NAME="${BASE_NET}_N${N}_${SEED}"
+NET_DIR="${BASE_NET_DIR}/${NET_NAME}"
+NETWORK="${NET_DIR}/${NET_NAME}.txt"
+
+ORDER="${NET_DIR}/${ATTACK}/oi_list.txt"
+echo ${ORDER}
+if [ -f "${ORDER}" ]; then
+    if [ ! -s "${ORDER}" ]; then
+        echo "File ${ORDER} has 0 size and it will be removed."
+        rm ${ORDER}
+    fi
+fi
+
+if [ `wc -l ${ORDER} | awk '{print $1}'` -lt $(echo ${N} | awk '{printf("%d\n", 0.9*$1)}') ]; then
+    echo "File ${ORDER} is too short"
+    exit 0  
+fi  
+
+if [ ! -f "${ORDER}" ]; then 
+    echo "File ${ORDER} does not exist"
+    exit 0
+fi
+
+OUTPUT="${NET_DIR}/${ATTACK}/comp_data.txt"
+if [ -f "${OUTPUT}" ]; then
+    if [ ! -s "${OUTPUT}" ]; then
+        echo "File ${OUTPUT} has 0 size and it will be removed."
+        rm ${OUTPUT}
+    fi
+fi
+
+if [ "${OVERWRITE}" != "True" ]; then
+    if [ -f "${OUTPUT}" ]; then 
+        echo "File ${OUTPUT} already exist"
+        exit 0
+    fi
+fi
+
+../perc_v2 $NETWORK $ORDER $OUTPUT 
