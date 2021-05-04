95daa91dbf0272b339c04069d7e69794c57a0669
nahuelalmeira <nahu1990@gmail.com>
2021-05-04 14:29:18 -0300

    Add networkit for IB

--- python/dismantling.py
+++ python/dismantling.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import queue
+import logging
 import numpy as np
 import igraph as ig
-import queue
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'fast'))
-from functions import RD_attack, RCI_attack, graph_to_nn_set
+from functions import get_CI, RD_attack, RCI_attack, graph_to_nn_set
+from auxiliary import ig_graph_to_adjlist
 
 def initial_attack(
     g, attack, out=None,
@@ -29,16 +31,31 @@ def initial_attack(
     else:
         ## Compute centrality
         if attack == 'Btw':
-            c_values = g.betweenness(directed=False, nobigint=False)
+            if isinstance(g, ig.Graph):
+                c_values = g.betweenness(directed=False, nobigint=False)
+            else:
+                import networkit as netKit
+                btw = netKit.centrality.Betweenness(g)
+                btw.run()
+                ranking = btw.ranking()
+                indices, c_values = zip(*list(sorted(ranking, key=lambda x: x[0])))
+                c_values = [c/2 for c in c_values]
         elif 'Btw_cutoff' in attack:
             cutoff = int(attack.split('cutoff')[1])
-            c_values = g.betweenness(directed=False, nobigint=False, cutoff=cutoff)
+            c_values = g.betweenness(
+                directed=False, nobigint=False, cutoff=cutoff
+            )
         elif attack == 'Deg':
             c_values = g.degree()
         elif attack == 'Eigenvector':
             from igraph import arpack_options
             arpack_options.maxiter = 300000
-            c_values = g.eigenvector_centrality(directed=False, arpack_options=arpack_options)
+            c_values = g.eigenvector_centrality(
+                directed=False, arpack_options=arpack_options
+            )
+        elif attack == 'CI':
+            adjlist = ig_graph_to_adjlist(g)
+            c_values = get_CI(adjlist, l=1)
         original_indices = np.argsort(c_values)[::-1]
 
     if out:
@@ -69,7 +86,14 @@ def edge_initial_attack(g, attack, out=None, random_state=0):
 
     return tuple_values
 
-def updated_attack_ig(graph, attack, out=None, random_state=0):
+def updated_attack_ig(
+    graph,
+    attack,
+    save_centrality=False, 
+    out=None, 
+    out_centrality=None,
+    random_state=0
+):
 
     ## Set random seed for reproducibility
     np.random.seed(random_state)
@@ -78,7 +102,6 @@ def updated_attack_ig(graph, attack, out=None, random_state=0):
     g = graph.copy()
     if 'BtwWU' in attack:
         g.es['weight'] = graph.es['weight']
-        #print(g.es['weight'])
 
     ## Save original index as a vertex property
     N0 = g.vcount()
@@ -86,6 +109,7 @@ def updated_attack_ig(graph, attack, out=None, random_state=0):
 
     ## List with the node original indices in removal order
     original_indices = []
+    centrality_stats = []
 
     j = 0
     if out:
@@ -97,8 +121,15 @@ def updated_attack_ig(graph, attack, out=None, random_state=0):
             #j += len(oi_values)
             j += oi_values.size
             np.savetxt(out, oi_values, fmt='%d')
-
         f = open(out, 'a+')
+    if save_centrality:
+        if not out_centrality:
+            raise ValueError('Missing value for out_centrality')
+        if os.path.isfile(out_centrality) and os.path.getsize(out_centrality) > 0:
+                centrality_stats = np.loadtxt(
+                    out_centrality, comments='\x00'
+                ).tolist()
+        f_centrality = open(out_centrality, 'a+')
 
     while j < N0:
 
@@ -106,52 +137,88 @@ def updated_attack_ig(graph, attack, out=None, random_state=0):
         if attack == 'BtwU':
             c_values = g.betweenness(directed=False, nobigint=False)
         elif attack == 'BtwWU':
-            #print(g.es['weight'])
             weights = g.es['weight'] if g.ecount() else None
-            c_values = g.betweenness(directed=False, weights=weights, nobigint=False)
+            c_values = g.betweenness(
+                directed=False, weights=weights, nobigint=False
+            )
         elif 'BtwU_cutoff' in attack:
             cutoff = int(attack.split('cutoff')[1])
-            c_values = g.betweenness(directed=False, nobigint=False, cutoff=cutoff)
+            c_values = g.betweenness(
+                directed=False, nobigint=False, cutoff=cutoff
+            )
         elif 'BtwWU_cutoff' in attack:
             cutoff = int(attack.split('cutoff')[1])
             weights = g.es['weight'] if g.ecount() else None
-            c_values = g.betweenness(directed=False, weights=weights, nobigint=False, cutoff=cutoff)
+            c_values = g.betweenness(
+                directed=False, weights=weights, nobigint=False, cutoff=cutoff
+            )
         elif attack == 'DegU':
             c_values = g.degree()
         elif attack == 'EigenvectorU':
             from igraph import arpack_options
             arpack_options.maxiter = 300000
-            c_values = g.eigenvector_centrality(directed=False, arpack_options=arpack_options)
+            c_values = g.eigenvector_centrality(
+                directed=False, arpack_options=arpack_options
+            )
 
-        #idx = np.argmax(c_values)
         c_values = np.around(c_values, decimals=8)
         m = np.max(c_values)
+        if c_values.shape[0] > 1:
+            m_sec = np.sort(c_values)[-2]
+        else:
+            m_sec = np.NaN
+        mean = np.mean(c_values)
+        c_stats = [m, mean, m_sec]
         m_indices = [i for i, value in enumerate(c_values) if value==m]
 
         idx = np.random.choice(m_indices)
-        #if j < 10:
-        #    print(j, idx, len(m_indices), m_indices, sorted(c_values, reverse=True)[:4])
-
 
         ## Add index to list
         original_idx = g.vs[idx]['name']
         original_indices.append(original_idx)
+        centrality_stats.append(c_stats)
 
         ## Remove node
         g.vs[idx].delete()
 
         j += 1
 
-        if out:
+        if out_centrality:
             f.write('{:d}\n'.format(original_idx))
             f.flush()
+            f_centrality.write(
+                ','.join([str(elem) for elem in c_stats]) + '\n'
+            )
+            f_centrality.flush()
 
     if out:
         f.close()
+    if out_centrality:
+        f_centrality.close()
 
-    return original_indices
+    return original_indices, centrality_stats
 
-def updated_attack_nk(g, attack, out=None, random_state=0):
+
+
+def updated_attack_nk(
+    g, 
+    attack, 
+    save_centrality=False, 
+    out=None, 
+    out_centrality=None,
+    random_state=0
+):
+
+    def perform_step(g, attack):
+        if attack == 'BtwU':
+            btw = netKit.centrality.Betweenness(g)
+            btw.run()
+            ranking = btw.ranking()
+            c_values = np.array([elem[1]/2 for elem in ranking])
+            idx = ranking[0][0]
+            c_value, mean_c = c_values.max(), c_values.mean()
+            c_stats = [c_value, mean_c]
+        return idx, c_stats
 
     import networkit as netKit
 
@@ -159,27 +226,32 @@ def updated_attack_nk(g, attack, out=None, random_state=0):
     np.random.seed(random_state)
 
     order = []
-
+    centrality_stats = []
     if out:
         if os.path.isfile(out) and os.path.getsize(out) > 0:
             oi_values = np.loadtxt(out, dtype='int', comments='\x00')
             if oi_values.size == 1:
                 oi_values = np.array([oi_values], dtype='int')
+            if save_centrality:
+                if not out_centrality:
+                    raise ValueError('Missing value for out_centrality')
+                centrality_stats = np.loadtxt(
+                    out_centrality, comments='\x00'
+                ).tolist()
             for oi in oi_values:
                 g.removeNode(oi)
                 order.append(oi)
 
         f = open(out, 'a+')
+        f_centrality = open(out_centrality, 'a+')
 
     N = g.numberOfNodes()
     while N:
 
-        if attack == 'BtwU':
-            btw = netKit.centrality.Betweenness(g)
-            btw.run()
-            idx, c_value = btw.ranking()[0]
+        idx, c_stats = perform_step(g, attack)
 
         order.append(idx)
+        centrality_stats.append(c_stats)
         g.removeNode(idx)
 
         N -= 1
@@ -187,17 +259,43 @@ def updated_attack_nk(g, attack, out=None, random_state=0):
         if out:
             f.write('{:d}\n'.format(idx))
             f.flush()
+            f_centrality.write(
+                ','.join([str(elem) for elem in c_stats]) + '\n'
+            )
+            f_centrality.flush()
 
     if out:
         f.close()
+        f_centrality.close()
 
-    return order
+    return order, centrality_stats
 
-def updated_attack(graph, attack, out=None, random_state=0):
+def updated_attack(
+    graph, 
+    attack, 
+    out=None, 
+    save_centrality=False,
+    out_centrality=None, 
+    random_state=0
+):
 
     if isinstance(graph, ig.Graph):
-        return updated_attack_ig(graph, attack, out, random_state)
-    return updated_attack_nk(graph, attack, out, random_state)
+        return updated_attack_ig(
+            graph, 
+            attack, 
+            out=out, 
+            save_centrality=save_centrality, 
+            out_centrality=out_centrality, 
+            random_state=random_state
+        )
+    return updated_attack_nk(
+            graph, 
+            attack, 
+            out=out, 
+            save_centrality=save_centrality, 
+            out_centrality=out_centrality, 
+            random_state=random_state
+        )
 
 def edge_updated_attack(graph, attack, out=None, random_state=0):
 
@@ -217,7 +315,8 @@ def edge_updated_attack(graph, attack, out=None, random_state=0):
     if out:
         if os.path.isfile(out) and os.path.getsize(out) > 0:
             tuple_list = np.loadtxt(out, dtype='int', comments='\x00')
-            tuple_list = np.array(tuple_list) ## In case oi_values is one single integer
+            ## In case oi_values is one single integer
+            tuple_list = np.array(tuple_list) 
             tuple_list = [(s,t) for (s,t) in tuple_list]
             g.delete_edges(tuple_list)
             j += len(tuple_list)
@@ -262,7 +361,8 @@ def fast_updated_attack(g, attack, out=None, random_state=0):
     if out:
         if os.path.isfile(out) and os.path.getsize(out) > 0:
             oi_values = np.loadtxt(out, dtype='int', comments='\x00')
-            oi_values = np.array(oi_values) ## In case oi_values is one single integer
+            ## In case oi_values is one single integer
+            oi_values = np.array(oi_values) 
             if len(oi_values) < N0:
                 os.remove(out)
 
@@ -326,7 +426,13 @@ def updated_local_attack(graph, attack, out=None, random_state=0):
 
     return original_indices
 
-def updated_hybrid_attack(graph, attacks, probabilities, out=None, random_state=0):
+def updated_hybrid_attack(
+    graph, 
+    attacks, 
+    probabilities, 
+    out=None, 
+    random_state=0
+):
 
     ## Set random seed for reproducibility
     np.random.seed(random_state)
@@ -377,7 +483,9 @@ def updated_hybrid_attack(graph, attacks, probabilities, out=None, random_state=
             elif attack == 'DegU':
                 c_values = g.degree()
             elif attack == 'EigenvectorU':
-                c_values = g.eigenvector_centrality(directed=False, arpack_options=arpack_options)
+                c_values = g.eigenvector_centrality(
+                    directed=False, arpack_options=arpack_options
+            )
             idx = np.argmax(c_values)
 
         ## Add index to list
@@ -409,20 +517,8 @@ def get_index_list(
     Write to output out index list in order of removal
     """
 
-    #if G.is_directed():
-    #    print('ERROR: G must be undirected.', file=sys.stderr)
-    #    return 1
-
-    #if not G.is_connected():
-    #    print('ERROR: G must be connected.', file=sys.stderr)
-    #    return 1
-
-    #if not G.is_simple():
-    #    print('ERROR: G must be simple.', file=sys.stderr)
-    #    return 1
-
     supported_attacks = {
-        'initial': ['Ran', 'Deg', 'Btw', 'Eigenvector'] + \
+        'initial': ['Ran', 'Deg', 'CI', 'Btw', 'Eigenvector'] + \
                    ['Btw_cutoff{}'.format(i) for i in range(2, 1000)],
         'updated': ['BtwU', 'EigenvectorU', 'BtwWU'] + \
                    ['BtwU_cutoff{}'.format(i) for i in range(2, 1000)] + \
@@ -432,7 +528,6 @@ def get_index_list(
         'edge_initial': ['Edge_Ran'],
         'edge_updated': ['Edge_BtwU']
     }
-
     if attack in supported_attacks['initial']:
         index_list = initial_attack(
             G, attack, out=out, save_centrality=save_centrality,
@@ -440,35 +535,54 @@ def get_index_list(
             random_state=random_state
         )
     elif attack in supported_attacks['updated']:
-        index_list = updated_attack(G, attack, out=out, random_state=random_state)
+        index_list = updated_attack(
+            G, attack, out=out, save_centrality=save_centrality,
+            out_centrality=out_centrality,
+            random_state=random_state
+        )
     elif attack in supported_attacks['updated_local']:
-        index_list = updated_local_attack(G, attack, out=out, random_state=random_state)
+        index_list = updated_local_attack(
+            G, attack, out=out, random_state=random_state
+        )
     elif attack in supported_attacks['fast_updated']:
-        index_list = fast_updated_attack(G, attack, out=out, random_state=random_state)
+        index_list = fast_updated_attack(
+            G, attack, out=out, random_state=random_state
+        )
     elif attack in supported_attacks['edge_initial']:
-        index_list = edge_initial_attack(G, attack, out=out, random_state=random_state)
+        index_list = edge_initial_attack(
+            G, attack, out=out, random_state=random_state
+        )
     elif attack in supported_attacks['edge_updated']:
-        index_list = edge_updated_attack(G, attack, out=out, random_state=random_state)
+        index_list = edge_updated_attack(
+            G, attack, out=out, random_state=random_state
+        )
     else:
-        print('ERROR: Attack {} not supported.'.format(attack),
-              file=sys.stderr)
+        logging.error(f'Attack {attack} not supported.')
 
     return index_list
 
 
-def get_index_list_hybrid(G, attacks, probabilities, out=None, random_state=0):
+def get_index_list_hybrid(
+    G, 
+    attacks, 
+    probabilities, 
+    out=None, 
+    random_state=0
+):
     """
     Write to output out index list in order of removal
     """
 
     if G.is_directed():
-        print('ERROR: G must be undirected.', file=sys.stderr)
+        logging.error('G must be undirected.')
         return 1
 
     if not G.is_simple():
-        print('ERROR: G must be simple.', file=sys.stderr)
+        logging.error('G must be simple.')
         return 1
 
-    index_list = updated_hybrid_attack(G, attacks, probabilities, out, random_state=random_state)
+    index_list = updated_hybrid_attack(
+        G, attacks, probabilities, out, random_state=random_state
+    )
 
     return index_list
