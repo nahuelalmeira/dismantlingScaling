61128bf5fcd6ff088289f25840ac5fb0740ba192
nahuelalmeira <nahu1990@gmail.com>
2021-05-05 10:06:17 -0300

    no andaba

--- python/auxiliary.py
+++ python/auxiliary.py
@@ -1,66 +1,97 @@
 import os
-import sys
 import errno
 import tarfile
 import pickle
 import json
+import logging
 import igraph as ig
 import numpy as np
 from planar import spatial_net_types, distance
+from typing import Iterable, Tuple, Optional
 
 simple_props = ['Ngcc', 'C', 'Cws', 'r', 'meank', 'D', 'meanl', 'meanlw']
+
+def ig_graph_to_adjlist(G):
+    """
+    >>> G = ig.Graph()
+    >>> G.add_vertices(3)
+    >>> G.add_edges([(0, 1), (1, 2)])
+    >>> ig_graph_to_adjlist(G)
+    [{1}, {0, 2}, {1}]
+    """
+    n = G.vcount()
+    adjlist = [set([]) for _ in range(n)]
+    for e in G.es():
+        s, t = e.tuple
+        adjlist[s].add(t)
+        adjlist[t].add(s)
+    return adjlist
+
+def edgelist_to_adjlist(
+    edgelist: Iterable[Tuple[int, int]],
+    size: Optional[int] = None
+) -> Iterable:
+    """
+    >>> # Empty list
+    >>> edgelist = []
+    >>> edgelist_to_adjlist(edgelist)
+    []
+    >>> # P4 graph
+    >>> edgelist = [(0, 1), (1, 2), (2, 3)]
+    >>> edgelist_to_adjlist(edgelist)
+    [{1}, {0, 2}, {1, 3}, {2}]
+    >>> # Accept lists instead of tuples
+    >>> edgelist = [[0, 1], [1, 2], [2, 3]]
+    >>> edgelist_to_adjlist(edgelist)
+    [{1}, {0, 2}, {1, 3}, {2}]
+    >>> # C4 graph
+    >>> edgelist = [(0, 1), (1, 2), (2, 3), (3, 0)]
+    >>> edgelist_to_adjlist(edgelist)
+    [{1, 3}, {0, 2}, {1, 3}, {0, 2}]
+    """
+    if len(edgelist) == 0 and not size:
+        return []
+
+    n = size if size else np.max(edgelist) + 1
+
+    adjlist = [set() for _ in range(n)]
+    for source, target in edgelist:
+        adjlist[source].add(target)
+        adjlist[target].add(source)
+
+    return adjlist
 
 def get_property_file_name(prop, directory):
     if prop in simple_props:
         file_name = os.path.join(directory, prop + '_values.txt')
     return file_name
 
-def get_base_network_name(net_type, size, param, **kwargs):
-    N = int(size)
-
+def get_base_network_name(net_type, size, param):
     if net_type == 'ER':
-        k = float(param)
-        base_net_name = 'ER_k{:.2f}'.format(k)
+        base_net_name = 'ER_k{:.2f}'.format(float(param))
     elif net_type == 'RR':
-        k = int(param)
-        base_net_name = 'RR_k{:02d}'.format(k)
+        base_net_name = 'RR_k{:02d}'.format(float(param))
     elif net_type == 'BA':
-        m = int(param)
-        base_net_name = 'BA_m{:02d}'.format(m)
-    elif net_type == 'Lattice':
-        L = int(size)
-        base_net_name = 'Lattice_param'
-    elif net_type == 'PLattice':
-        L = int(size)
-        base_net_name = 'PLattice_param'
-    elif net_type == 'Ld3':
-        L = int(size)
-        base_net_name = 'Ld3_param'
+        base_net_name = 'BA_m{:02d}'.format(int(param))
     elif net_type == 'MR':
-        if 'rMST' in kwargs and kwargs['rMST']:
+        if 'k' in param:
+            base_net_name = 'MR_k{:.2f}'.format(float(param[1:]))
+        elif 'rMST' == param:
             base_net_name = 'MR_rMST'
-        elif 'meank' in kwargs and kwargs['meank']:
-            k = float(param)
-            base_net_name = 'MR_k{:.2f}'.format(k)
         else:
-            r = float(param)
-            base_net_name = 'MR_r{:.6f}'.format(r)
-    elif net_type == 'DT':
-        base_net_name = 'DT_param'
-    elif net_type == 'PDT':
-        base_net_name = 'PDT_param'
-    elif net_type == 'GG':
-        base_net_name = 'GG_param'
-    elif net_type == 'RN':
-        base_net_name = 'RN_param'
+            base_net_name = 'MR_r{:.6f}'.format(float(param[1:]))
+    elif net_type in ['Lattice', 'PLattice', 'Ld3', 'DT', 'PDT', 'GG', 'RN']:
+        base_net_name = f'{net_type}_param'
+    elif net_type == 'qDT':
+        base_net_name = 'qDT_k{:.2f}'.format(float(param))
     else:
-        print('ERROR: net_type not supported', file=sys.stderr)
+        logging.error(f'{net_type} not supported')
         base_net_name = ''
 
     if net_type in ['Lattice', 'PLattice', 'Ld3']:
-        base_net_name_size = base_net_name + '_L{}'.format(L)
+        base_net_name_size = base_net_name + '_L{}'.format(int(size))
     else:
-        base_net_name_size = base_net_name + '_N{}'.format(N)
+        base_net_name_size = base_net_name + '_N{}'.format(int(size))
     return base_net_name, base_net_name_size
 
 supported_attacks = [
@@ -75,12 +106,14 @@ supported_attacks += ['Edge_Ran', 'Edge_BtwU']
 
 def get_edge_weights(g, net_type, size, param, seed):
     if net_type not in spatial_net_types:
-        print('Network type not supported for this attack')
+        logging.info('Network type not supported for this attack')
 
     N = size
 
     base_net_dir_name = '../networks/DT/DT_param/DT_param_N{}'.format(N)
-    net_dir_name = os.path.join(base_net_dir_name, 'DT_param_N{}_{:05d}'.format(N, seed))
+    net_dir_name = os.path.join(
+        base_net_dir_name, 'DT_param_N{}_{:05d}'.format(N, seed)
+    )
     position_file_name = 'position.txt'
     full_position_file_name = os.path.join(net_dir_name, position_file_name)
 
@@ -88,7 +121,7 @@ def get_edge_weights(g, net_type, size, param, seed):
     tar_input_name = 'position.tar.gz'
     full_tar_input_name = os.path.join(net_dir_name, tar_input_name)
     if not os.path.exists(full_tar_input_name):
-        print('ERROR: File', full_tar_input_name, 'does not exist')
+        logging.error('File ' + full_tar_input_name + ' does not exist')
     tar = tarfile.open(full_tar_input_name, 'r:gz')
     tar.extractall(net_dir_name)
     tar.close()
@@ -103,8 +136,15 @@ def get_edge_weights(g, net_type, size, param, seed):
 
     return edge_weights
 
-def read_data_file(directory, base_name, reader, file_ext='.txt', compress_ext='.tar.gz'):
-    """Auxiliary function for reading common data files, which could be compressed.
+def read_data_file(
+    directory, 
+    base_name, 
+    reader, 
+    file_ext='.txt', 
+    compress_ext='.tar.gz'
+):
+    """Auxiliary function for reading common data files, 
+    which could be compressed.
 
     Arguments:
         directory {[type]} -- [description]
@@ -127,14 +167,13 @@ def read_data_file(directory, base_name, reader, file_ext='.txt', compress_ext='
         elif reader == 'networkit':
             import networkit as netKit
             return netKit.readGraph(
-                file_name, fileformat=netKit.Format.EdgeListSpaceZero, directed=False
+                file_name, fileformat=netKit.Format.EdgeListSpaceZero, 
+                directed=False
             )
 
     compress_file_name = base_name + compress_ext
     full_compress_file_name = os.path.join(directory, compress_file_name)
-    tar_exist = False
-    if os.path.isfile(full_compress_file_name):
-        tar_exist = True
+    tar_exist = os.path.isfile(full_compress_file_name)
 
     data_file_name = base_name + file_ext
     full_data_file_name = os.path.join(directory, data_file_name)
@@ -160,14 +199,14 @@ def read_data_file(directory, base_name, reader, file_ext='.txt', compress_ext='
 
 
 def get_number_of_nodes(net_type, size):
-    if net_type in ['ER', 'RR', 'BA', 'MR', 'DT', 'PDT']:
-        N = int(size)
-    elif net_type in ['Lattice', 'PLattice']:
+    if net_type in ['Lattice', 'PLattice']:
         L = int(size)
         N = L*L
     elif net_type == 'Ld3':
         L = int(size)
         N = L*L*L
+    else:
+        N = int(size)
     return N
 
 ####################################################
@@ -198,20 +237,27 @@ def get_max_pos(dir_name):
     return max_pos, delta_max
 
 
-def load_delta_data(net_type, size, param, attack, seed):
+def load_delta_data(net_type, size, param, attack, seed, **kwargs):
 
-    dir_name = os.path.join('../networks', net_type)
-    base_net_name, base_net_name_size = get_base_network_name(net_type, size, param)
+    dir_name = os.path.join('../networks', net_type)  
+    base_net_name, base_net_name_size = get_base_network_name(
+        net_type, size, param
+    )
     net_name = base_net_name_size + '_{:05d}'.format(seed)
     base_net_dir = os.path.join(dir_name, base_net_name, base_net_name_size)
     net_dir = os.path.join(base_net_dir, net_name)
 
     if net_type in ['PDT', 'GG', 'RN', 'MR']:
-        ## Directory corresponding to DT (in case other spatial network is used)
+        ## Directory corresponding to DT 
+        ## (in case other spatial network is used)
         DT_dir_name = os.path.join('../networks', 'DT')
-        DT_base_net_name, DT_base_net_name_size = get_base_network_name('DT', size, param)
+        DT_base_net_name, DT_base_net_name_size = (
+            get_base_network_name('DT', size, param)
+        )
         DT_net_name = DT_base_net_name_size + '_{:05d}'.format(seed)
-        DT_base_net_dir = os.path.join(DT_dir_name, DT_base_net_name, DT_base_net_name_size)
+        DT_base_net_dir = os.path.join(
+            DT_dir_name, DT_base_net_name, DT_base_net_name_size
+        )
         pos_net_dir = os.path.join(DT_base_net_dir, DT_net_name)
     else:
         pos_net_dir = net_dir
@@ -231,34 +277,18 @@ def load_delta_data(net_type, size, param, attack, seed):
 
 
 def get_prop(g, prop):
-
-    if prop == 'C':
-        return g.transitivity_undirected(mode='zero')
-
-    if prop == 'Cws':
-        return g.transitivity_avglocal_undirected(mode='zero')
-
-    if prop == 'r':
-        return g.assortativity_degree(directed=False)
-
-    if prop == 'D':
-        return g.diameter(directed=False)
-
-    if prop == 'meanl':
-        return g.average_path_length(directed=False)
-
-    if prop == 'meanlw':
-        return np.mean(g.shortest_paths(weights='weight', mode=ig.ALL))
-
-    if prop == 'meank':
-        return np.mean(g.degree())
-
-    if prop == 'maxk':
-        return max(g.degree())
-
-    if prop == 'Btw_dist':
-        return g.betweenness(directed=False, nobigint=False)
-
+    prop_dict = {
+        'C':        g.transitivity_undirected(mode='zero'),
+        'Cws':      g.transitivity_avglocal_undirected(mode='zero'),
+        'r':        g.assortativity_degree(directed=False),
+        'D':        g.diameter(directed=False),
+        'meanl':    g.average_path_length(directed=False),
+        'meanlw':   np.mean(g.shortest_paths(weights='weight', mode=ig.ALL)),
+        'meank':    np.mean(g.degree()),
+        'maxk':     np.max(g.degree()),
+        'Btw_dist': g.betweenness(directed=False, nobigint=False)
+    }
+    return prop_dict[prop]
 
 def save_json_data(data, file_name):
     with open(file_name, 'w') as f:
@@ -276,10 +306,23 @@ def save_pickle_data(data, file_name):
 def powerlaw(X, a, c):
     return c*np.array(X)**a
 
-def getLinearReg(sizes, values, t=1):
+def getLinearReg(sizes, values, t=1, scale='loglog'):
 
-    X = np.log(sizes)
-    Y = np.log(values)
+    if scale == 'loglog':
+        X = np.log(sizes)
+        Y = np.log(values)
+    elif scale == 'logy':
+        X = np.array(sizes)
+        Y = np.log(values)
+    elif scale == 'logx':
+        X = np.log(sizes)
+        Y = np.array(values)
+    elif scale == 'linear':
+        X = np.array(sizes)
+        Y = np.array(values)
+    else:
+        raise ValueError('ERROR: scale', scale, 'not supported')
+
     coeffs, cov = np.polyfit(X, Y, 1, cov=True)
     errors = np.sqrt(np.diag(cov))
 
